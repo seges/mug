285b5586826cde084d7aa260b3217c5b93f9dc4f
Ladislav Gazo <gazo@seges.sk>
2020-05-04 10:14:06 +0200

    openjdk11 java image

--- docker/backend-java/entrypoint.sh
+++ docker/backend-java/entrypoint.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
-if [ -d /docker-entrypoint.d ]; then
-	for f in /docker-entrypoint.d/*.sh; do
-		[ -f "$f" ] && . "$f"
-	done
+#if [ -d /docker-entrypoint.d ]; then
+#	for f in /docker-entrypoint.d/*.sh; do
+#		[ -f "$f" ] && . "$f"
+#	done
+#fi
+
+echo "RUN_SSH=$RUN_SSH"
+echo "RUN_IN_FOREGROUND=$RUN_IN_FOREGROUND"
+
+if [ "$RUN_SSH" = "true" ]; then
+  sudo /etc/init.d/ssh start
 fi
 
-su - developer
+if [ "$RUN_IN_FOREGROUND" = "true" ]; then
+  tail -f /dev/null
+fi
+
